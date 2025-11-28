b3b49a2dae8f126829e3dcbdb35009f4019c4272
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2025-11-28 12:34:17 -0600

    added scripts dir to projects_menu.sh - Fri Nov 28 12:34:17 PM CST 2025

--- rofi-scripts/projects_menu.sh
+++ rofi-scripts/projects_menu.sh
@@ -3,18 +3,26 @@ set -eu
 
 # Set your terminal:
 TERM="ghostty"
+
+# Directories
 DEV="$HOME/Documents/dev"
+CHEZMOI_DIR="$HOME/.local/share/chezmoi"
+SCRIPT_DIR="$HOME/Documents/scripts"
 
 # Pick repo
 configs="$(find "$DEV"/*/ -maxdepth 0 -print0 | xargs -0 -n1 basename)"
 [ -n "$configs" ] || exit 0
-chezmoi="$(basename ~/.local/share/chezmoi)"
+chezmoi="$(basename "$CHEZMOI_DIR")"
 [ -n "$chezmoi" ] || exit 0
-chosen="$(printf '%s\n' "$configs" "$chezmoi" |
+scripts="$(basename "$SCRIPT_DIR")"
+[ -n "$scripts" ] || exit 0
+chosen="$(printf '%s\n' "$configs" "$chezmoi" "$scripts" |
   rofi -dmenu -p 'Projects:')"
 [ -n "$chosen" ] || exit 0
 if [[ "$chosen" == "chezmoi" ]]; then
-  dir="$HOME/.local/share/chezmoi"
+  dir="$CHEZMOI_DIR"
+elif [[ "$chosen" == "scripts" ]]; then
+  dir="$SCRIPT_DIR"
 else
   dir="$DEV/$chosen"
 fi
